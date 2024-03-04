a7e2e8feedc92d14688cdad77516570855101c87
shaogz <shaogz@bokecc.com>
2024-03-04 18:09:18 +0800

    work steal pool

--- src/com/brew/home/thread/pool/Demo2.java
+++ src/com/brew/home/thread/pool/Demo2.java
@@ -1,5 +1,7 @@
 package com.brew.home.thread.pool;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -9,44 +11,29 @@ import java.util.concurrent.TimeUnit;
  */
 public class Demo2 {
 
-    public final int a;
-
-    private int value = 0;
-
-    public Demo2(int a) {
-        this.a = a;
-    }
-
-    public Demo2() {
-        this.a = -1;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
 
     public static void main(String[] args) throws Exception {
-        Demo2 demo2 = new Demo2();
 
-        //未有结论...
-        CompletableFuture.runAsync(() -> {
-            System.out.println(1);
-            try {
-                TimeUnit.SECONDS.sleep(120);
-            }catch (Exception e) {
+        //以下测试证明：使用work steal pool时，提交任务不会阻塞
+        //因为其本身也有一个WorkQueue[]存放等待执行的任务。
+        //所以不建议使用CompletableFuture.runAsync的原因就只剩下：场景和模糊的执行机制
+        //100的原因是本机是20核心，所以得大一点...
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (int i = 0 ; i < 100; i++) {
+            System.out.println(simpleDateFormat.format(new Date()) + " 提交任务 " + i);
+            int finalI = i;
+            CompletableFuture.runAsync(() -> {
+                System.out.println(simpleDateFormat.format(new Date()) + " 任务开始执行 " + finalI);
+                try {
+                    TimeUnit.SECONDS.sleep(120);
+                }catch (Exception e) {
+                }
+                System.out.println(simpleDateFormat.format(new Date()) + " 任务执行完毕 " + finalI);
+            });
+        }
 
-            }
-        });
-
-        System.out.println(1);
         TimeUnit.SECONDS.sleep(1200);
-
-
-
+        System.out.println("ALL DONE!");
 
     }
 }
